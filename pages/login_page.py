3f6299da8399df6ddd4989cd1d33e1fc580ce5d8
Svetlana <svetlana@192.168.1.6>
2022-11-21 23:29:49 +0300

    Initial commit

---
+++ pages/login_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def go_to_login(self):
+        login_email = self.browser.find_element(*LoginPageLocators.LOGIN_EMAIL)
+        login_email.send_keys('svetatest1@gmail.com')
+
+    def go_to_password(self):
+        login_password = self.browser.find_element(*LoginPageLocators.LOGIN_PASS)
+        login_password.send_keys('svetatest1')
+
+    def go_to_login_button(self):
+        login_button = self.browser.find_element(*LoginPageLocators.LOGIN_BTN)
+        login_button.submit()
+
